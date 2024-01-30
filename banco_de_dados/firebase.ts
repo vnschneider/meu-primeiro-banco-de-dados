d3aedf83624a72f519d439ef1b68a52e265313fc
Vinícius Schneider <mrschneider.v@gmail.com>
2024-01-30 17:35:11 -0300

    revisao da aula

--- banco_de_dados/firebase.ts
+++ banco_de_dados/firebase.ts
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import * as firestore from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyARC_Zxq40khYiyiZ357DXsjY8ggYJ_aRk",
   authDomain: "teste-1c96d.firebaseapp.com",
@@ -14,9 +10,10 @@ const firebaseConfig = {
   appId: "1:766998349001:web:478aaa644e02217e6cf918",
 };
 
-// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+
+export const db = firestore.getFirestore(firebase);
+
 console.log("Conectado ao Firebase!");
-const Firebase = initializeApp(firebaseConfig);
-export const db = firestore.getFirestore(Firebase);
 
 export { firestore };
