033cd4d00c40709484f3006f296e312753f1f789
Vinícius Schneider <mrschneider.v@gmail.com>
2024-01-31 17:41:38 -0300

    criacao de rota

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
-import express from 'express';
-import { db, firestore } from '../banco_de_dados/firebase';
+import express from "express";
+import { db, firestore } from "../banco_de_dados/firebase";
 
 const app = express();
 
-app.listen(3000, function(){
- console.log("Servidor rodando na porta http://localhost:3000")
+app.get("/", (req, res) => {
+  res.send("Bem vindo a minha primeira API");
+});
+
+app.post("/usuario", async (req, res) => {
+  const usuario = req.body.nome;
+
+  try {
+    const docRef = await firestore.addDoc(
+      firestore.collection(db, "usuarios"),
+      {
+        nome: nome,
+      }
+    );
+
+    res.send(docRef.id);
+  } catch (e) {
+    console.log(e);
+
+    res.status(500).send(e);
+  }
+});
+
+app.listen(3000, function () {
+  console.log("Servidor rodando na porta http://localhost:3000");
 });
