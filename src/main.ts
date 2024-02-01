6b5d99c820fb09c3e24dd53288a2b928a7d20e4d
Vinícius Schneider <mrschneider.v@gmail.com>
2024-01-31 21:46:12 -0300

    refactor

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,14 @@ import { db, firestore } from "../banco_de_dados/firebase";
 
 const app = express();
 
+// TODO: Inserir no código para utilização do json na requisição
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Bem vindo a minha primeira API");
 });
 
 app.post("/usuario", async (req, res) => {
-  console.log('requisição: '+req.body);
-
   const nome = req.body.nome;
 
   try {
@@ -19,7 +20,7 @@ app.post("/usuario", async (req, res) => {
         nome: nome,
       }
     );
-
+    //TODO: Adicionar textos de apoio para a melhor leitura das  respostas
     res.send("Usuario adicionado com sucesso: " + docRef.id);
   } catch (e) {
     console.log("Erro ao adicionar usuário: ", e);
