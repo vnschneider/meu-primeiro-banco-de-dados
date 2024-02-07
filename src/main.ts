4cfb965968560b679182be03886fdea0e37c9276
Vinícius Schneider <mrschneider.v@gmail.com>
2024-02-07 17:12:13 -0300

    deletei  um usuario

--- src/main.ts
+++ src/main.ts
@@ -66,6 +66,20 @@ app.put('/atualizarUsuario/:id', async (req, res) => {
   }
 })
 
+app.delete('/deletarUsuario/:id', async (req, res) =>{
+  const id = req.params.id
+
+  try {
+    await firestore.deleteDoc(firestore.doc(db, 'usuarios', id))
+
+    res.send('Usuário deletado com sucesso!')
+  } catch (e) {
+    console.log('Erro ao deletar usuario: ' + e)
+
+    res.status(500).send('Erro ao deletar usuario: ' + e)
+  }
+})
+
 app.listen(3000, function () {
   console.log("Servidor rodando em http://localhost:3000");
 });
